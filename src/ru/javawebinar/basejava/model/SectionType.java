c1d4f576d34362d4dd5ab272c5da7f7f07814c93
Viktoria Tomchuk <viktoria.kostyuk.1293@gmail.com>
2019-07-25 01:28:24 +0300

    Lesson 7

---
+++ src/ru/javawebinar/basejava/model/SectionType.java
@@ -0,0 +1,21 @@
+package ru.javawebinar.basejava.model;
+
+public enum SectionType {
+    PERSONAL("Личные качества"),
+    OBJECTIVE("Позиция"),
+    ACHIEVEMENT("Достижения"),
+    QUALIFICATION("Квалификация"),
+    EXPERIENCE("Опыт роботы"),
+    EDUCATION("Образование");
+
+    private String title;
+
+    SectionType(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+}
