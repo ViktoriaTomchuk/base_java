b5d19991ae51fbe6474e29a653299633dcb6f643
Viktoria Tomchuk <viktoria.kostyuk.1293@gmail.com>
2019-09-07 19:48:00 +0300

    Lesson 10. HW9 (with problem moments in StorageException)

--- src/ru/javawebinar/basejava/exception/StorageException.java
+++ src/ru/javawebinar/basejava/exception/StorageException.java
@@ -3,6 +3,8 @@ package ru.javawebinar.basejava.exception;
 public class StorageException extends RuntimeException {
     private final String uuid;
 
+
+//    Додала самостійно, щоб уникнути помилок в інших файлах
     public StorageException(String message, Object o) {
         this(message, null, null);
 
